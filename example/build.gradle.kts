73c19e5dc5e22a2af6a334928a0b6c3a69cc45de
sya-ri <git@s7a.dev>
2025-08-11 05:23:06 +0900

    feat: Add Minecraft server testing tasks

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,6 +1,64 @@
+import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask
+import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask.JarUrl
+
+plugins {
+    alias(libs.plugins.minecraft.server)
+}
+
 dependencies {
     compileOnly(libs.spigot.api)
 
     implementation(project(":api"))
     implementation(project(":runtime"))
+}
+
+listOf(
+    "17" to "1.17.1",
+    "18" to "1.18",
+    "18_1" to "1.18.1",
+    "18_2" to "1.18.2",
+    "19" to "1.19",
+    "19_1" to "1.19.1",
+    "19_2" to "1.19.2",
+    "19_3" to "1.19.3",
+    "19_4" to "1.19.4",
+    "20" to "1.20",
+    "20_1" to "1.20.1",
+    "20_2" to "1.20.2",
+    "20_3" to "1.20.3",
+    "20_4" to "1.20.4",
+    "20_6" to "1.20.6",
+    "21" to "1.21",
+    "21_1" to "1.21.1",
+    "21_3" to "1.21.3",
+    "21_4" to "1.21.4",
+    "21_5" to "1.21.5",
+).forEach { (name, version) ->
+    tasks.register<LaunchMinecraftServerTask>("testPlugin$name") {
+        dependsOn("build")
+
+        doFirst {
+            copy {
+                from(
+                    layout.buildDirectory.asFile
+                        .get()
+                        .resolve("libs/${project.name}.jar"),
+                )
+                into(
+                    layout.buildDirectory.asFile
+                        .get()
+                        .resolve("MinecraftServer$name/plugins"),
+                )
+            }
+        }
+
+        serverDirectory.set(
+            layout.buildDirectory.asFile
+                .get()
+                .resolve("MinecraftServer$name")
+                .absolutePath,
+        )
+        jarUrl.set(JarUrl.Paper(version))
+        agreeEula.set(true)
+    }
 }
--- example/src/main/kotlin/dev/s7a/ktAdvancements/example/ExamplePlugin.kt
+++ example/src/main/kotlin/dev/s7a/ktAdvancements/example/ExamplePlugin.kt
@@ -2,9 +2,36 @@ package dev.s7a.ktAdvancements.example
 
 import dev.s7a.ktAdvancements.KtAdvancementStore
 import dev.s7a.ktAdvancements.KtAdvancements
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 @Suppress("unused")
 class ExamplePlugin : JavaPlugin() {
-    private val ktAdvancement = KtAdvancements(Advancement.entries, KtAdvancementStore.InMemory())
+    private val ktAdvancements = KtAdvancements(Advancement.entries, KtAdvancementStore.InMemory())
+
+    override fun onEnable() {
+        server.pluginManager.registerEvents(
+            object : Listener {
+                @EventHandler
+                fun on(event: PlayerJoinEvent) {
+                    val player = event.player
+                    ktAdvancements.grant(player, Advancement.HelloWorld)
+                }
+
+                @EventHandler
+                fun on(event: BlockBreakEvent) {
+                    val player = event.player
+                    val block = event.block
+                    if (block.type == Material.STONE) {
+                        ktAdvancements.grant(player, Advancement.MineStone, step = 1)
+                    }
+                }
+            },
+            this,
+        )
+    }
 }
